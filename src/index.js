3253f07bbf32f81b824bf2ea76e5a8dcb5246f25
Tymur Bondar <tymurbondar@outlook.com>
2023-09-14 22:38:14 -0400

    3 challenges

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
-console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+const breedUrl = "https://dog.ceo/api/breeds/list/all";
+const imgContainer = document.getElementById("dog-image-container");
+const doglist = document.getElementById("dog-breeds");
+
+fetch(imgUrl)
+    .then((response) => response.json())
+    .then((image) => image.message.forEach(function (imageUrl) {
+        const img = document.createElement("img");
+        img.src = imageUrl;
+        imgContainer.appendChild(img);
+    })
+    )
+
+fetch(breedUrl)
+    .then((response) => response.json())
+    .then((dogs) => {
+        for (const breed in dogs.message) {
+            const subBread = dogs.message[breed];
+            const li = document.createElement("li");
+            li.innerText = breed;
+            li.classList.add("changeColor");
+            if (Array.isArray(subBread) && subBread.length > 0) {
+                for (let i = 0; i < subBread.length; i++) {
+                    const subLi = document.createElement("li");
+                    subLi.innerText = `${breed}  ${subBread[i]}`;
+                    doglist.appendChild(subLi);
+                }
+            }
+            else {
+                doglist.appendChild(li);
+            }
+        }
+    })
+
+
+doglist.addEventListener("click", (event) => {
+    event.target.style.color = 'red';
+})
